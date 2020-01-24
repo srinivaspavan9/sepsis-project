057b0cc28410f7ae09d6ba6f5d844f641c948eb2
srinivaspavan9 <srinivaspavan05@gmail.com>
2020-01-24 20:27:38 +0530

    second commit

--- awareness/urls.py
+++ awareness/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='awareness-home'),
+    path('',views.home,name='home'),
+    path('base/',views.base,name='base')
 ]
--- prediction/views.py
+++ prediction/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+import pandas as pd
+import pickle
+from sklearn.externals import joblib
+import pickle
+from sklearn.preprocessing import MinMaxScaler
 
-# Create your views here.
+
+def formhandle(request):
+    if(request.method=='POST'):
+        gen=request.POST['gender']
+        hr=request.POST['heartrate']
+        temp= request.POST['temp']
+        age = request.POST['age']
+        oxy = request.POST['oxygen']
+        bp = request.POST['bp']
+        resp = request.POST['resp']
+        iculos = request.POST['iculos']
+        time = request.POST['time']
+        mymodel=joblib.load('C:\\Users\\srinivas pavan\\PycharmProjects\\Prediction\\seppred\\sepsis\\log_model.pkl')
+        mydf = pd.DataFrame({'Gender': int(gen), 'custom_hr': hr, 'custom_temp': temp, 'custom_age': age,
+                             'custom_o2stat': oxy, 'custom_bp': bp, 'custom_resp': resp, 'ICULOS': int(iculos),
+                             'HospAdmTime': -2.45}, index=[0])
+        print(mydf)
+        val=mymodel.predict(mydf)
+        if val==[0]:
+            val='No chance of sepsis'
+        else:
+            val='Chances of sepsis are high'
+        print('myvalue :'+str(val))
+        return render(request,'prediction/prediction.html',{'val':val})
+    else:
+        return render(request,'prediction/prediction.html')
+
